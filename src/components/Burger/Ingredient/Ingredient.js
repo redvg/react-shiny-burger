448199e0b1bd9cba2d5bc2ac493dbe4bad0dd639
redvg <redvg17@gmail.com>
2018-07-25 16:32:23 +0200

    adds ingredients

---
+++ src/components/Burger/Ingredient/Ingredient.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+
+const ingredient = (props) => {
+
+    let ingredient = null;
+
+    switch (props.ingredient){
+
+        case 'breadBottom':
+
+            ingredient = <div className={classes.BreadBottom}></div>;
+
+            break;
+
+        case 'breadTop':
+
+            ingredient = 
+                <div>
+                    <div className={classes.Seeds1}></div>
+                    <div className={classes.Seeds2}></div>
+                </div>;
+
+            break;
+
+        case 'meat':
+
+            ingredient = <div className={classes.BreadBottom}></div>;
+
+            break;
+
+        case 'cheese':
+
+            ingredient = <div className={classes.Cheese}></div>;
+
+            break;
+
+        case 'bacon':
+
+            ingredient = <div className={classes.Bacon}></div>;
+
+            break;
+
+        case 'salad':
+
+            ingredient = <div className={classes.salad}></div>;
+
+            break;
+    }
+
+    return ingredient;
+}
+
+export default ingredient;
