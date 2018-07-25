1736ab193cfa1c8361f74defe1ca4c358a6d4e6b
redvg <redvg17@gmail.com>
2018-07-25 19:08:25 +0200

    outputs ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,13 +2,25 @@ import React from 'react';
 import Ingredient from './Ingredient/Ingredient';
 import classes from './Burger.css';
 
-const burger = () =>
-    <div className={classes.Burger}>
-        <Ingredient ingredient="breadTop"/>
-        <Ingredient ingredient="salad"/>
-        <Ingredient ingredient="cheese"/>
-        <Ingredient ingredient="meat"/>
-        <Ingredient ingredient="breadBottom"/>
-    </div>
+const burger = (props) => {
+
+    let parsedIngredients = Object.keys( props.ingredients )
+        .map( igKey => {
+            return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
+                return <Ingredient key={igKey + i} ingredient={igKey} />;
+            } );
+        } )
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+        
+    if (parsedIngredients.length === 0) {
+        parsedIngredients = <p>Please start adding ingredients!</p>;
+    }
+
+    return <div className={classes.Burger}> {parsedIngredients} </div>
+
+}
+    
 
 export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,11 +5,25 @@ import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component{
 
+    state = {
+        
+        ingredients: {
+
+            salad: 1,
+
+            cheese: 2,
+
+            meat: 1,
+
+            bacon: 4,
+        },
+    }
+
     render () {
 
         return (
             <Aux>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Burger controls</div>
             </Aux>
         );
