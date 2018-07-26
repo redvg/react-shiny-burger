b40a9032765d1f0c3159e92a7fb123a16454b45b
redvg <redvg17@gmail.com>
2018-07-26 22:08:13 +0200

    fixes alt for img

--- src/components/Navigation/Logo/Logo.js
+++ src/components/Navigation/Logo/Logo.js
@@ -4,7 +4,8 @@ import burgerLogo from '../../../assets/images/burger-logo.png';
 
 const logo = () =>
     <div className={styles.Logo}>
-        <img src={burgerLogo}/>
+        <img src={burgerLogo}
+             alt='logo'/>
     </div>
 
 export default logo;
