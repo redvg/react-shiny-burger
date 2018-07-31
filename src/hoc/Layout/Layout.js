788fcde1267b6b74c19d5c2a185a558901a609a7
redvg <redvg17@gmail.com>
2018-07-31 23:26:18 +0200

    moves layout to hoc

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import Aux from '../../hoc/Auxiliary/Auxiliary';
+import Aux from '../Auxiliary/Auxiliary';
 import classes from './Layout.css'
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import Sidedrawer from '../Navigation/Sidedrawer/Sidedrawer';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import Sidedrawer from '../../components/Navigation/Sidedrawer/Sidedrawer';
 
 class Layout extends Component{
 
