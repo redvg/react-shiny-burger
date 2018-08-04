0faf7e2298cb72469ec54aa7e70e339a76b85f80
redvg <redvg17@gmail.com>
2018-08-04 11:31:51 +0200

    adds routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import {BrowserRouter, Route} from 'react-router-dom';
+
 
 class App extends Component {
 
@@ -9,10 +11,12 @@ class App extends Component {
 
     return (     
       
-      <Layout>
-        <BurgerBuilder />
-        <Checkout />
-      </Layout>
+      <BrowserRouter>
+        <Layout>
+          <Route path='/checkout' exact component={Checkout} />
+          <Route path='/' exact component={BurgerBuilder} />          
+        </Layout>
+      </BrowserRouter>
     
     );
   }
--- src/components/Navigation/NavigationBar/NavigationBar.js
+++ src/components/Navigation/NavigationBar/NavigationBar.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from './NavigationBar.css';
-import NavigationItem from './NavigationItem/NavigationItem';
+//import NavigationItem from './NavigationItem/NavigationItem';
+import {Link} from 'react-router-dom';
 
 const navigationBar = () =>
     <ul className={styles.NavigationBar}>
-        <NavigationItem link ='/' isActive>builder</NavigationItem>
-        <NavigationItem link='/'>checkout</NavigationItem>
+        <Link to='/'>builder</Link>
+        <Link to='/checkout'>checkout</Link>
+        {/*<NavigationItem link ='/' isActive>builder</NavigationItem>
+        <NavigationItem link='/checkout'>checkout</NavigationItem>*/}
     </ul>
 
 
