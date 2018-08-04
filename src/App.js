51d66bbc1ad317eedc61f4f79e9444c54ada77da
redvg <redvg17@gmail.com>
2018-08-04 17:20:06 +0200

    moves router to index

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 
 
 class App extends Component {
 
   render() {
 
-    return (     
-      
-      <BrowserRouter>
-        <Layout>
-          <Route path='/checkout' exact component={Checkout} />
-          <Route path='/' exact component={BurgerBuilder} />          
-        </Layout>
-      </BrowserRouter>
+    return (
+
+    <Layout>
+        <Route path='/checkout' exact component={Checkout} />
+        <Route path='/' exact component={BurgerBuilder} />          
+    </Layout>
     
     );
   }
--- src/components/Navigation/NavigationBar/NavigationBar.js
+++ src/components/Navigation/NavigationBar/NavigationBar.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import styles from './NavigationBar.css';
-//import NavigationItem from './NavigationItem/NavigationItem';
 import {Link} from 'react-router-dom';
 
 const navigationBar = () =>
     <ul className={styles.NavigationBar}>
         <Link to='/'>builder</Link>
         <Link to='/checkout'>checkout</Link>
-        {/*<NavigationItem link ='/' isActive>builder</NavigationItem>
-        <NavigationItem link='/checkout'>checkout</NavigationItem>*/}
     </ul>
 
 
