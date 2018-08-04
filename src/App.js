975b23249223bb5a36e20bbf1c0c2b83e0a6f7ce
redvg <redvg17@gmail.com>
2018-08-04 17:21:22 +0200

    adds routes' switch

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 
 
 class App extends Component {
@@ -11,11 +11,12 @@ class App extends Component {
 
     return (
 
-    <Layout>
-        <Route path='/checkout' exact component={Checkout} />
-        <Route path='/' exact component={BurgerBuilder} />          
-    </Layout>
-    
+      <Switch>
+        <Layout>
+          <Route path='/checkout' exact component={Checkout} />
+          <Route path='/' exact component={BurgerBuilder} />          
+        </Layout>
+      </Switch>
     );
   }
 }
